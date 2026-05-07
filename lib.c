c8dc84eaa27fed1d38a9feeaee961f5fd43f90fa
Oleg Mishunin <rjawiygvozd@gmail.com>
2026-05-07 21:40:40 +0700

    helper macros

--- lib.c
+++ lib.c
@@ -2,6 +2,27 @@
 #include <stdlib.h>
 #include "../hs.h"
 
+#define setByValue(TYPE, NAME, VALUE)\
+TYPE NAME = VALUE;\
+api->set(st, #NAME, &NAME, sizeof(TYPE))
+
+#define setByPointer(TYPE, NAME, VALUE)\
+TYPE NAME = VALUE;\
+api->set(st, #NAME, NAME, sizeof(TYPE))
+
+#define getValue(TYPE, NAME)\
+TYPE NAME;\
+api->get(st, #NAME, &NAME)
+
+#define getPointer(TYPE, NAME)\
+TYPE NAME;\
+api->get(st, #NAME, NAME)
+
+#define call(NAME, ARG1VALUE)\
+api->callFunction(st, #NAME, (void*)ARG1VALUE)
+
+int _i = 0;
+
 shellState *st;
 shellApi *api;
 
